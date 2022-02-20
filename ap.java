178ee3be586303bd65ad28234a14d7b84e173b90
Ashutosh-Puri <98416234+Ashutosh-Puri@users.noreply.github.com>
2022-02-20 11:42:47 +0530

    Create ap.java

---
+++ ap.java
@@ -0,0 +1,22 @@
+// perimeter and area of rectangle
+// a=l*b; p=2*(l+b);
+
+public class ap
+ {
+	public static void main(String[] args)
+	 {
+	   double a,l ,b , p;
+
+	   l=5;
+	   b=65;
+	   
+	   a=l*b;
+	   p=2*(l+b);
+	   
+	   System.out.println("Area of Rectangle :"+a);
+     System.out.println("Perimeter of Rectangle :"+p);
+ 	}
+}
+// o/p :
+//Area of Rectangle :325.0
+//Perimeter of Rectangle :140.0
