15b3673ad8f976414d17a04b93e2f4f0db1598ce
Ashutosh-Puri <98416234+Ashutosh-Puri@users.noreply.github.com>
2022-02-20 11:50:43 +0530

    Create vc.java

---
+++ vc.java
@@ -0,0 +1,17 @@
+// Calculate the volume of cylinder. (hint : Volume: π × r² × h)
+// v= pi*r²*h;
+public class vc
+ {
+	public static void main(String[] args) 
+	{    
+        double  r=66;
+        double  h=15;
+	
+        double v = Math.PI*(r*r)*h;
+        
+        System.out.println("Volume of Cylinder :"+v);				
+	}
+}
+
+
+// o/p : Volume of Cylinder :205271.6639855571
